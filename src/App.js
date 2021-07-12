3db095b957d59d4ca3914456c7e1d591a58e6aed
MananSharma12 <manan.sharma1205@gmail.com>
2021-07-12 12:10:49 +0530

    first commit"

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HomePage from './pages/homepage/homepage.component'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
