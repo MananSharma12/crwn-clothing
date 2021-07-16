2eecab1768fcad3df1a51dd088b52e5fcbd123b7
MananSharma12 <manan.sharma1205@gmail.com>
2021-07-17 04:34:39 +0530

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -29,8 +29,7 @@ class App extends React.Component {
               id: snapshot.id,
               ...snapshot.data()
             }
-          })
-          console.log(this.state);
+          });
         });
       } else {
         this.setState({ currentUser: userAuth });
